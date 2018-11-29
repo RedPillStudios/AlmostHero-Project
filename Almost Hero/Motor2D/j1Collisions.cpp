536eb0337c09e2601eb7055e4f8665432c1aa4ac
lucho1 <luchosuaya@hotmail.com>
2018-11-29 10:38:03 +0100

    Heavy Crash fixed

--- Almost Hero/Motor2D/j1Collisions.cpp
+++ Almost Hero/Motor2D/j1Collisions.cpp
@@ -8,6 +8,15 @@
 j1Collisions::j1Collisions()
 {
 
+	for (int i = 0; i < MAX_COLLIDERS; i++)
+		colliders[i] = nullptr;
+	
+
+	for (int i = 0; i < COLLIDER_MAX; i++) {
+		for (int j = 0; j < COLLIDER_MAX; j++)
+			matrix[i][j] = false;
+	}
+
 	//Set all matrix positions to false
 	for (int i = 0; i < COLLIDER_MAX; i++) {
 		for (int j = 0; j < COLLIDER_MAX; j++)
